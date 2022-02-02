1794dd8bc43a9bd5e9f4574aa8b04eb1a8319da1
AthenaCaesura <athena.caesura@zapatacomputing.com>
2022-02-02 01:46:11 -0500

    feat: error correction in register b

--- test_present_results.py
+++ test_present_results.py
@@ -5,19 +5,18 @@ from srb import mem_qubit_reset, srb_memory
 
 plot_shots(
     1,
-    mem_err_param=0.1,
     reg_b_copies=1,
-    filename="tests/1_copy",
+    filename="tests/test2",
 )
-plot_shots(
-    1,
-    mem_err_param=0.1,
-    reg_b_copies=3,
-    filename="tests/3_copy",
-)
-plot_shots(
-    1,
-    mem_err_param=0.1,
-    reg_b_copies=5,
-    filename="tests/5_copy",
-)
+# plot_shots(
+#     1,
+#     mem_err_param=0.1,
+#     reg_b_copies=3,
+#     filename="tests/3_copy",
+# )
+# plot_shots(
+#     1,
+#     mem_err_param=0.1,
+#     reg_b_copies=5,
+#     filename="tests/5_copy",
+# )
